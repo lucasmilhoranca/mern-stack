40f7e356ec8ce3be844a09718c7733dd1b6faf78
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2023-11-06 17:47:38 -0300

    refactor: update and middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const userService = require("../services/user.service");
-const mongoose = require('mongoose');
+//const mongoose = require("mongoose");
 
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
@@ -38,18 +38,31 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-    const id = req.params.id;
+  const user = req.user;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({ message: "Invalid Id" });
-    }
-    const user = await userService.findByIdService(id);
-
-    if (!user) {
-        return res.status(400).send({ message: "User not found" });
-    }
-
-    res.send(user);
+  res.send(user);
 };
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at least one fild for update" });
+  }
+
+  const { id, user } = req;
+
+  await userService.updateService(
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+  );
+
+  res.send({ message: "User succesfully update" });
+};
+
+module.exports = { create, findAll, findById, update };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,8 +6,23 @@ const findAllService = () => user.find();
 
 const findByIdService = (id) => user.findById(id);
 
+const updateService = (
+  id,
+  name,
+  username,
+  email,
+  password,
+  avatar,
+  background
+) =>
+  user.findOneAndUpdate(
+    { _id: id },
+    { name, username, email, password, avatar, background }
+  );
+
 module.exports = {
-    createService,
-    findAllService,
-    findByIdService
+  createService,
+  findAllService,
+  findByIdService,
+  updateService,
 };
