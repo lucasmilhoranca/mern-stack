7625acdfcc5a65965c2447ec212b76cca393a9bf
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2023-11-10 20:22:14 -0300

    middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,67 +2,83 @@ const userService = require("../services/user.service");
 //const mongoose = require("mongoose");
 
 const create = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
+  try {
+    const { name, username, email, password, avatar, background } = req.body;
 
-  if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({ message: "Submit all filds for registration" });
+    if (!name || !username || !email || !password || !avatar || !background) {
+      res.status(400).send({ message: "Submit all filds for registration" });
+    }
+
+    const user = await userService.createService(req.body);
+
+    if (!user) {
+      return req.status(400).send({ message: "Error creating User" });
+    }
+
+    res.status(201).send({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        name: name,
+        username,
+        email,
+        avatar,
+        background,
+      },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  const user = await userService.createService(req.body);
-
-  if (!user) {
-    return req.status(400).send({ message: "Error creating User" });
-  }
-
-  res.status(201).send({
-    message: "User created successfully",
-    user: {
-      id: user._id,
-      name: name,
-      username,
-      email,
-      avatar,
-      background,
-    },
-  });
 };
 
 const findAll = async (req, res) => {
-  const users = await userService.findAllService();
+  try {
+    const users = await userService.findAllService();
 
-  if (users.length === 0) {
-    return res.status(400).send({ message: "There are no registered users" });
+    if (users.length === 0) {
+      return res.status(400).send({ message: "There are no registered users" });
+    }
+
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  res.send(users);
 };
 
 const findById = async (req, res) => {
-  const user = req.user;
+  try {
+    const user = req.user;
 
-  res.send(user);
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
 const update = async (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body;
+  try {
+    const { name, username, email, password, avatar, background } = req.body;
 
-  if (!name && !username && !email && !password && !avatar && !background) {
-    res.status(400).send({ message: "Submit at least one fild for update" });
+    if (!name && !username && !email && !password && !avatar && !background) {
+      res.status(400).send({ message: "Submit at least one fild for update" });
+    }
+
+    const { id, user } = req;
+
+    await userService.updateService(
+      id,
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      background
+    );
+
+    res.send({ message: "User succesfully update" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  const { id, user } = req;
-
-  await userService.updateService(
-    id,
-    name,
-    username,
-    email,
-    password,
-    avatar,
-    background
-  );
-
-  res.send({ message: "User succesfully update" });
 };
 
 module.exports = { create, findAll, findById, update };
